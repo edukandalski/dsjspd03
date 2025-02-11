274c1a9b6ed8aae6f6d318c589589bf93cead9cb
Eduardo Kandalski <edukandalski@gmail.com>
2025-02-11 20:31:49 -0300

    Versão inicial, somente boilerplate do spring.

---
+++ src/main/java/com/eduardokandalski/dsjspd03/Dsjspd03Application.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dsjspd03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsjspd03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsjspd03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardokandalski/dsjspd03/Dsjspd03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dsjspd03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsjspd03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

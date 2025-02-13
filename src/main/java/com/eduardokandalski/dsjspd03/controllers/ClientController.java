a99e92c2617a0ca2f9af70b514aac246ed1c6df0
Eduardo Kandalski <edukandalski@gmail.com>
2025-02-12 21:25:24 -0300

    Implementado findAll com uso de Pageable para paginação

--- src/main/java/com/eduardokandalski/dsjspd03/controllers/ClientController.java
+++ src/main/java/com/eduardokandalski/dsjspd03/controllers/ClientController.java
@@ -3,12 +3,13 @@ package com.eduardokandalski.dsjspd03.controllers;
 import com.eduardokandalski.dsjspd03.dto.ClientDTO;
 import com.eduardokandalski.dsjspd03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -20,5 +21,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ClientDTO findById(@PathVariable Long id) {
         return clientService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return clientService.findAll(pageable);
     }
 }
--- src/main/java/com/eduardokandalski/dsjspd03/services/ClientService.java
+++ src/main/java/com/eduardokandalski/dsjspd03/services/ClientService.java
@@ -4,10 +4,12 @@ import com.eduardokandalski.dsjspd03.dto.ClientDTO;
 import com.eduardokandalski.dsjspd03.entities.Client;
 import com.eduardokandalski.dsjspd03.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -19,5 +21,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = clientRepository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
